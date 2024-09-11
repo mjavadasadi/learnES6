0569cd0e955b88fee50fda82c6c9ffeffa3d4d72
mjavadasadi <asadij313@gmail.com>
2024-09-11 23:49:35 +0330

    debug

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-import {addUser,renderusers} from './user'
-
-const users = [];
+import { User, renderusers } from "./user.js";
 
 
-
+const users = [{name:"ahmadReza",age:14},{name:"roham",age:15}];
 const updateUserList = (user) => {
   users = [...users, user];
   renderusers();
 };
-const displayUser=(name ,age)=>{
 
-  return `اسم:${name} , سن : ${age}`
 
-}
 
-// console.log(displayUser("ahmadreza", "14"));
+
+const editUser = ( newName, newAge) => {
+  User.edit(newName, newAge);
+  renderusers();
+};
+const deleteUser =(user)=>{
+users=users.filter(u => u!==user);
+renderusers();
+};
+
+// console.log(displayUser("ahmadrez", "14"));
 // console.log(displayUser("javad", "34"));
--- user.js
+++ user.js
@@ -1,7 +1,25 @@
+
+export class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  edit(newName, newAge) {
+    this.name = newName;
+    this.age = newAge;
+  }
+  delete(userArray) {
+    return userArray.filter(user=> user!==this);
+  }
+}
+
 export const addUser = (name = "مهمان", age = "نامشخص") => {
   const user = { name, age };
   return user;
 };
+
+export const displayUser = ({name, age}) =>  `اسم:${name} , سن : ${age}`;
+
 
 export const renderusers = () => {
   const userlist = document.getElementById("userList");
@@ -9,3 +27,4 @@ export const renderusers = () => {
     (user) => `<li>${user.name} , ${user.age}</li>`
   );
 };
+
